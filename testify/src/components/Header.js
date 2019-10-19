e7d99cd45c549996e80d05c9e76ff0a728b9c176
Jordan Miller <jordan.j.miller@live.com>
2019-10-19 16:42:43 -0700

    Added test.css to separate debug styling. Added functional header component, needs to be fleshed out but works as intended showing different nav links based on bools/state.

--- testify/src/components/Header.js
+++ testify/src/components/Header.js
@@ -1,15 +1,61 @@
-import React from 'react'
+import React, {useState} from 'react'
 import { Link, NavLink } from 'react-router-dom';
+import styled from 'styled-components';
+import '../test.css'
 
 export default function Header() {
+  const HDiv = styled.div`
+  text-decoration: none;
+  `;
+  const StyledNavLink = styled(NavLink)`
+  text-decoration: none;
+  color: black;
+  :visited{
+    color: black;
+  }
+  :hover{
+    background-color: grey;
+  }
+  `;
+  const StyledLink = styled(Link)`
+  text-decoration: none;
+  color: black;
+  :visited{
+    color: black;
+  }
+  `;
+  
+  const [loggedIn, setLoggedIn] = useState(false)
+
     return (
-        <div>
-            <h1 className='initial'>Header</h1>
-            <Link to='/'>Home </Link>
-            <Link to='/Login'>Sign in </Link>
-            <Link to='/Teacher'>Teacher </Link>
-            <Link to='/Student'>Student </Link>
-        </div>
+        <HDiv className='headerDiv'>
+            <StyledLink exact to='/'><h1 className='initial'>Testify</h1></StyledLink>
+
+            {(()=>{
+            if ( loggedIn === true){
+              return <nav>
+                <StyledNavLink exact to='/'>Home </StyledNavLink>
+                <StyledNavLink to='/Login'>Sign in </StyledNavLink>
+                <StyledNavLink to='/Teacher'>Teacher </StyledNavLink>
+                <StyledNavLink to='/Student'>Student </StyledNavLink>
+              </nav>
+            }
+            else if (loggedIn === false){
+              return <nav>
+                <StyledNavLink exact to='/'>Home </StyledNavLink>
+                <StyledNavLink to='/Login'>Sign in </StyledNavLink>
+              </nav>
+            }
+          })()}
+
+          
+        <div><button onClick={ ()=>{ 
+          if (loggedIn){setLoggedIn(false)} 
+          else{setLoggedIn(true)} 
+          } }>Logged In: {`${loggedIn}`}</button></div>
+
+            
+        </HDiv>
     )
 }
 
