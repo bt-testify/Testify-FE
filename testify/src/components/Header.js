40506004d4a2b79349530260226c012087bf9ed9
Jordan Miller <jordan.j.miller@live.com>
2019-10-19 15:13:11 -0700

    created new branch, working on building out the Header component

--- testify/src/components/Header.js
+++ testify/src/components/Header.js
@@ -1,14 +1,14 @@
 import React from 'react'
-import { Link } from 'react-router-dom';
+import { Link, NavLink } from 'react-router-dom';
 
 export default function Header() {
     return (
         <div>
             <h1 className='initial'>Header</h1>
-            <Link to='/'>Home</Link>
-            <Link to='/login'>Sign in</Link>
-            <Link to='/Teacher'>Teacher</Link>
-            <Link to='/Student'>Student</Link>
+            <Link to='/'>Home </Link>
+            <Link to='/Login'>Sign in </Link>
+            <Link to='/Teacher'>Teacher </Link>
+            <Link to='/Student'>Student </Link>
         </div>
     )
 }
