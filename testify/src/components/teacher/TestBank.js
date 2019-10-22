4bd628c6750504ef0ebc2a8eeea5272bbccc9a7e
Greg <gsc229@gmail.com>
2019-10-22 19:02:30 -0400

    getTestsByCreator sending back filtered array based on test creator name.

--- testify/src/components/teacher/TestBank.js
+++ testify/src/components/teacher/TestBank.js
@@ -1,12 +1,35 @@
-import React, { useEffect } from 'react';
+import React, { useEffect, useState } from 'react';
+import { Link } from 'react-router-dom';
 import { axiosWithAuth } from '../../utils/axiosWithAuth';
 
 export default function TestBank(props) {
   console.log('TestBank.js props: ', props);
+  const { assignedTests, classes, studentIds, name } = props.currentUser;
+  const [tests, setTests] = useState([]);
+
+  useEffect(() => {
+    axiosWithAuth()
+      .get(`/testsByCreator/${name}`)
+      .then(res => {
+        console.log('TestBank.js res', res);
+      })
+      .catch(err => {
+        console.log('TestBank.js err', err);
+      });
+  }, []);
+
   return (
     <div>
       <h1 className='initial'>Test Bank Component</h1>
-      <div className='teacher-classes'></div>
+      <div className='teacher-classes'>
+        <ul>
+          {classes.map((klass, index) => (
+            <li key={index}>
+              Tests for <a href='#'>{klass.subject} </a> class
+            </li>
+          ))}
+        </ul>
+      </div>
     </div>
   );
 }
