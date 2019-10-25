7199dd1d3119c133b45002a95fdb4f67dd207996
Greg <gsc229@gmail.com>
2019-10-25 08:25:55 -0400

    Updates

---
+++ precache-manifest.06930a0cac3a2ce3e8ff2ab41741bcf3.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f565813d17c0adf6392945436fe7785a",
+    "url": "/Front-End/index.html"
+  },
+  {
+    "revision": "80bfbd1988dafbb34d6b",
+    "url": "/Front-End/static/css/main.935b728c.chunk.css"
+  },
+  {
+    "revision": "62471b1671b96795091e",
+    "url": "/Front-End/static/js/2.a74ff637.chunk.js"
+  },
+  {
+    "revision": "80bfbd1988dafbb34d6b",
+    "url": "/Front-End/static/js/main.919610a6.chunk.js"
+  },
+  {
+    "revision": "7b014426be834e015f0a",
+    "url": "/Front-End/static/js/runtime-main.aeba9c51.js"
+  },
+  {
+    "revision": "da8d632bfa2f02750495fe37d38d7a6c",
+    "url": "/Front-End/static/media/Beachprofile.da8d632b.jpg"
+  },
+  {
+    "revision": "2493763ebaeea569d75ad11daf918109",
+    "url": "/Front-End/static/media/Greg_cameron.2493763e.png"
+  },
+  {
+    "revision": "f79fd79e461cd64fd9b7e4718aa925e8",
+    "url": "/Front-End/static/media/Jonathan_Ferrer.f79fd79e.png"
+  },
+  {
+    "revision": "43edd390e369768b60482c6232f3b89d",
+    "url": "/Front-End/static/media/Jordan_Miller.43edd390.png"
+  },
+  {
+    "revision": "edb686a01c34794bf9e296bacb2a20bb",
+    "url": "/Front-End/static/media/Raajn_Patel.edb686a0.png"
+  }
+]);
